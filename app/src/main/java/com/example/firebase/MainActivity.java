5ed9d9ce7e505261a4f1f94fe6892db11b8ef945
ClubBuddy <itismymessagebox@mail.ru>
2021-04-09 17:04:02 +0500

    lol

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,10 +20,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase firebaseDatabase;
+    FirebaseStorage storage;
     Button button;
     EditText editText1;
     EditText editText2;
@@ -37,12 +41,27 @@ public class MainActivity extends AppCompatActivity {
 
 
         firebaseDatabase = FirebaseDatabase.getInstance();
+        storage = FirebaseStorage.getInstance();
 
         button = findViewById(R.id.button);
         editText1 = findViewById(R.id.editTextTextPersonName2);
         editText2 = findViewById(R.id.editTextTextPersonName);
         textView = findViewById(R.id.textView);
         image = findViewById(R.id.imageView);
+
+        StorageReference storageRef = storage.getReference().child("mp3").child("kangi-ejya-mp3.mp3");
+        StorageReference songsRef = storageRef.child("songs");
+        StorageReference song1Ref = storageRef.child("songs/song1.mp3");
+
+        try {
+            MediaPlayer player = new MediaPlayer();
+            player.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            player.setDataSource("https://firebasestorage.googleapis.com/v0/b/firstproj-d32ba.appspot.com/o/mp3%2Fkangi-ejya-mp3.mp3?alt=media&token=ff61bf38-2c8c-4ffc-bff3-3e39fca0497b");
+            player.prepare();
+            player.start();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
 
         button.setOnClickListener(v -> {
             user = new User();
@@ -102,6 +121,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
     }
 
     private void onSignInSuccess() {
